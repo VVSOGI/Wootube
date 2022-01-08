72a718459922c7ec67bd398d7ded6f79bf6c7226
vvsogi <vvsogi@gmail.com>
2022-01-08 23:30:28 +0900

    feat/ basic setting

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import "./App.css";
 
 function App() {
+  const API_KEY = process.env.REACT_APP_YOUTUBE_API_KEY;
+
   return <div className="App">Hello world!</div>;
 }
 
